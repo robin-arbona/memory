9956662fcd30a05981c37e1a91e75a36fcab6092
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-01-28 11:15:58 +0100

    merge with between main and dev-robin-a

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -3,26 +3,19 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
 use App\Game\Game;
-=======
->>>>>>> origin/main
 use App\Manager\UsersManager;
 use Core\Manager;
-use http\Env\Request;
 
 class Controller extends Manager
 {
 
     public function signup($user_form)
     {
-<<<<<<< HEAD
-=======
         $new_user = new UsersManager();
-        if(isset($_POST['submit'])) {
+        if (isset($_POST['submit'])) {
             $new_user->add(htmlspecialchars($user_form['login']), password_hash($user_form['password'], PASSWORD_DEFAULT));
         }
->>>>>>> origin/main
     }
 
     public function getView($view, $data = [])
@@ -36,17 +29,14 @@ class Controller extends Manager
 
     public function login($user_form)
     {
-<<<<<<< HEAD
-=======
         $user = new UsersManager();
-        if($user->verify_user_login($user_form['login']) == true){
-            if($user->verify_user_password($user_form['login'],$user_form['password']) == true){
+        if ($user->verify_user_login($user_form['login']) == true) {
+            if ($user->verify_user_password($user_form['login'], $user_form['password']) == true) {
                 var_dump($_SESSION);
-                    $_SESSION['login'] = $user_form['login'];
-                    header('Location: index.php?view=game');
+                $_SESSION['login'] = $user_form['login'];
+                header('Location: index.php?view=game');
             }
         }
->>>>>>> origin/main
     }
 
     public function menu()
