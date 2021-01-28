97cec4f931cf13c74d3f75bc49d92bc9d761aa7d
pierre-malardier <pierre.malardier@laplateforme.io>
2021-01-28 11:12:53 +0100

    hashage password and login and submit usable

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -3,16 +3,19 @@
 
 namespace App\Controller;
 
-
 use App\Manager\UsersManager;
 use Core\Manager;
+use http\Env\Request;
 
 class Controller extends Manager
 {
 
-    public function signup()
+    public function signup($user_form)
     {
-
+        $new_user = new UsersManager();
+        if(isset($_POST['submit'])) {
+            $new_user->add(htmlspecialchars($user_form['login']), password_hash($user_form['password'], PASSWORD_DEFAULT));
+        }
     }
 
     public function getView($view)
@@ -23,9 +26,16 @@ class Controller extends Manager
         return $content;
     }
 
-    public function login()
+    public function login($user_form)
     {
-
+        $user = new UsersManager();
+        if($user->verify_user_login($user_form['login']) == true){
+            if($user->verify_user_password($user_form['login'],$user_form['password']) == true){
+                var_dump($_SESSION);
+                    $_SESSION['login'] = $user_form['login'];
+                    header('Location: index.php?view=game');
+            }
+        }
     }
 
     public function menu()
