59f50a3d210b8b6a0b93cc8fe3ec0bcaffbb4644
pierre-malardier <pierre.malardier@laplateforme.io>
2021-02-01 12:32:09 +0100

    mise en plce css

--- view/signup.php
+++ view/signup.php
@@ -2,9 +2,9 @@
 
 ?>
 
-
+<div class="container">
 <form class="row justify-content-center" method="post" action="index.php?view=signup">
-    <h1 class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">Sign Up</h1>
+    <h1>Sign Up</h1>
     <div class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">
         <label for="login" class="form-label">Login</label>
         <input name="login" type="text" class="form-control" id="login">
@@ -15,4 +15,4 @@
     </div>
     <button  name="submit" type="submit" class="btn btn-primary w-50">Submit</button>
 </form>
-
+</div>
