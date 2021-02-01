a4b8b692cdbdae22ea43a474995ca8f7760c807b
pierre-malardier <pierre.malardier@laplateforme.io>
2021-02-01 12:12:52 +0100

    bootstrap on form

--- view/login.php
+++ view/login.php
@@ -1,10 +1,16 @@
 <?php
 ?>
-<h1>Login</h1>
 
-<form method="post" action="index.php?view=login">
-    <input name="login" type="text">
-    <input name="password" type="password">
-    <button name="submit" type="submit">Submit</button>
+
+<form class="row justify-content-center" method="post" action="index.php?view=login">
+    <h1 class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">Login</h1>
+    <div class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">
+        <label for="login" class="form-label">Login</label>
+        <input name="login" type="text" class="form-control" id="login">
+    </div>
+    <div class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">
+        <label for="password" class="form-label">Password</label>
+        <input name="password" type="password" class="form-control" id="password">
+    </div>
+    <button name="submit" type="submit" class="btn btn-primary w-50">Submit</button>
 </form>
-
--- view/signup.php
+++ view/signup.php
@@ -1,11 +1,18 @@
 <?php
 
 ?>
-<h1>Sign Up</h1>
 
-<form method="post" action="index.php?view=signup">
-    <input name="login" type="text">
-    <input name="password" type="password">
-    <button name="submit" type="submit">Submit</button>
+
+<form class="row justify-content-center" method="post" action="index.php?view=signup">
+    <h1 class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">Sign Up</h1>
+    <div class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">
+        <label for="login" class="form-label">Login</label>
+        <input name="login" type="text" class="form-control" id="login">
+    </div>
+    <div class="col-11 col-sm-10 col-md-8 col-lg-6 m-5">
+        <label for="password" class="form-label">Password</label>
+        <input name="password" type="password" class="form-control" id="password">
+    </div>
+    <button  name="submit" type="submit" class="btn btn-primary w-50">Submit</button>
 </form>
 
